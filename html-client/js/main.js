bc8df233e5233f5c91c0f79c2721531d68abfc14
algo-ryth-nic <praanjjal.agnihotri@gmail.com>
2022-05-07 15:43:34 +0530

    changes to frontend

--- html-client/js/main.js
+++ html-client/js/main.js
@@ -1,67 +1,117 @@
-console.log("Hello World")
+console.log("Hello World");
 
 const url = new URL(window.location.href);
+console.log(url);
 
-
-const msgBox = document.getElementById('msg-box');
-msgBox.innerHTML = `
-<div class="alert alert-${url.searchParams.get("id") ? "success" : "danger"}" role="alert">
-<h3>id: ${url.searchParams.get("id") || "?"}</h3>
-${url.searchParams.get("id") ? "Data upload success" : "Data does not exist"}
-</div>
-${url.searchParams.get("id") ?
-        `<div id=\"spinner\" class=\"alert alert-info\" role=\"alert\">
-<div class='spinner-border'></div>
-Please wait while the data is being processed, You can save the Url to view the results later        
-</div>` : ""
-    }
-`;
-
-const getResult = async (id) => {    
-    const result = await fetch(`http://${url.host}/result/${id}`);
-    const resultObj = await result.json();
-    return resultObj;
-    
-}
+const getResult = async (id) => {
+  const result = await fetch(`http://${url.host}/result/${id}`);
+  const resultObj = await result.json();
+  return resultObj;
+};
 
 const plotData = (obj) => {
-    //console.log(JSON.parse(obj["frequent_items"]));
-    //console.log(JSON.parse(obj["association-rules"]));
-    const rules = JSON.parse(obj["association-rules"]);
-    const tdataEL =  document.getElementById("tabledata");
-    let str = "";
-    // rule['antecedents'] should not work , idk why it's working 
-    // not gonna touch it for now will look into later
-    rules.forEach(rule => {
-        str += `
+  //console.log(JSON.parse(obj["frequent_items"]));
+  //console.log(JSON.parse(obj["association-rules"]));
+  const rules = JSON.parse(obj["association-rules"]);
+  const tdataEL = document.getElementById("tabledata");
+  let str = "";
+  // rule['antecedents'] should not work , idk why it's working
+  // not gonna touch it for now will look into later
+  rules.forEach((rule) => {
+    str += `
         <tr>
-        <th scope="row">${rule['lift']}</th>
-        <td>${rule['antecedent support']}</td>
-        <td>${rule['consequent support']}</td>
-        <td>${rule['antecedents']}</td>
-        <td>${rule['consequents']}</td>
-        <td>${rule['confidence']}</td>
+        <th scope="row">${rule["lift"]}</th>
+        <td>${rule["antecedent support"]}</td>
+        <td>${rule["consequent support"]}</td>
+        <td>${rule["antecedents"]}</td>
+        <td>${rule["consequents"]}</td>
+        <td>${rule["confidence"]}</td>
         </tr>
-        `;        
-    });    
-    tdataEL.innerHTML = str;
-}
+        `;
+  });
+  tdataEL.innerHTML = str;
+};
 
 const plotDataHandler = async () => {
-    const obj = await getResult(url.searchParams.get("id"));
-    if(obj.length == 0){
-        // check for results in every 10 seconds if not yet processed
-        setTimeout(plotDataHandler, 10 * 1000);
-    }
-    else{
-        document.getElementById('spinner').innerHTML = "DATA PROCESSING COMPLETE";
-        plotData(obj[0]);
-    }
-}
+  const obj = await getResult(url.searchParams.get("id"));
+  if (obj.length == 0) {
+    // check for results in every 5 seconds
+    setTimeout(plotDataHandler, 5 * 1000);
+  } else {
+    // document.getElementById("spinner").innerHTML = "DATA PROCESSING COMPLETE";
+    // plots the data
+    plotData(obj[0]);
+  }
+};
 
+// on upload redirect
 if (url.searchParams.get("id")) {
-    document.getElementsByTagName('button')[0].innerHTML = "DATA UPLOADED";
-    document.getElementsByTagName('button')[0].disabled = true;
-    plotDataHandler();
+  const contentBox = document.querySelector(".menu-content-container");
+  contentBox.innerHTML = `</div>
+${
+  url.searchParams.get("id")
+    ? `     <div class="spinner-container">
+          <div class="spinner-border"></div>
+          <strong>Processing...</strong>
+        </div>`
+    : "Error"
+}`;
+  plotDataHandler();
 }
 
+// file upload & drag and drop related
+if (!url.search) {
+  const dropZone = document.querySelector("#drop-zone");
+
+  dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    console.log("file is in drop zone");
+    if (e.dataTransfer.types.indexOf("Files")) {
+      dropZone.classList.add("active");
+    }
+  });
+
+  dropZone.addEventListener("dragleave", () => {
+    dropZone.classList.remove("active");
+  });
+
+  dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    console.log("File(s) dropped");
+
+    if (e.dataTransfer.items) {
+      // Using DataTransferItemList interface to access the file(s)
+      console.log(e.dataTransfer.items[0]);
+      // If dropped item is a file
+      if (e.dataTransfer.items[0].kind === "file") {
+        const file = e.dataTransfer.items[0].getAsFile();
+        console.log("file[" + i + "].name = " + file.name);
+        validateFile(file) ? document.querySelector("form").submit() : console.log("invalid file");
+      }
+    }
+  });
+
+  document.querySelector("input[type=file]").addEventListener("change", (e) => {
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    validateFile(file) ? document.querySelector("form").submit() : console.log("invalid file");
+  });
+
+  document.querySelector("#browse-button").addEventListener("click", () => {
+    document.getElementById("file").click();
+  });
+
+  function validateFile(file) {
+    // file type validation
+    if (file.type !== "text/csv" && file.type !== "application/vnd.ms-excel") {
+      alert("Please upload a csv file or an excel file");
+      return false;
+    }
+    // file size validation
+    if (file.size > 10 ** 6) {
+      alert("Please upload a file less than 1 MB");
+      return false;
+    }
+    return true;
+  }
+}
--- node-server/src/index.js
+++ node-server/src/index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const multer = require('multer')
-const path = require('path')
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
 //DB-CONNECTION
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://mongodb:27017/";
+// const uri = "mongodb://mongodb:27017/";
+const uri = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 async function connectToDb() {
   try {
@@ -20,54 +21,61 @@ async function connectToDb() {
 connectToDb();
 //DB-CONNECTION
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath))
+// const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join("/home/pj/Documents/ml-project/html-client/");
+app.use(express.static(publicPath));
 
-const uploadPath = path.join(__dirname, '..', 'uploads');
+const uploadPath = path.join(__dirname, "..", "uploads");
+// const uploadPath = path.join(__dirname, "..", "uploads");
 const upload = multer({
   dest: uploadPath,
   fileFilter: function fileFilter(req, file, cb) {
     const ext = path.extname(file.originalname);
-    if (ext !== '.csv') {
-      return cb(null, false)
+    if (ext !== ".csv") {
+      return cb(null, false);
     }
-    cb(null, true)
+    cb(null, true);
   },
   limits: {
     // 1 MB LIMIT
-    fileSize: 1024 * 1024
-  }
-})
+    fileSize: 1024 * 1024,
+  },
+});
 
-const pythonCodeDir = "/home/app/python-related/";
+// const pythonCodeDir = "/home/app/python-related/";
+const pythonCodeDir = "/home/pj/Documents/ml-project/python-related";
 const pythonScript = "script_apriori.py";
-const pathToDataset = "/home/app/node-server/uploads/";
+// const pathToDataset = "/home/app/node-server/uploads/";
+const pathToDataset = "./uploads/";
 const minSupport = "0.05";
 
-
-app.post('/upload', upload.single('csvdata'), function (req, res, next) {
+app.post("/upload", upload.single("csvdata"), function (req, res, next) {
   if (req.file) {
-    exec(`python3 ${pythonCodeDir}${pythonScript} ${pathToDataset}${req.file.filename} ${req.file.filename} ${minSupport}`);
-    res.redirect(`http://${req.get('host')}/?msg=upload sucess&id=${req.file.filename}`);
+    exec(
+      `python3 ${pythonCodeDir}${pythonScript} ${pathToDataset}${req.file.filename} ${req.file.filename} ${minSupport}`
+    );
+    res.redirect(`http://${req.get("host")}/?msg=upload sucess&id=${req.file.filename}`);
+  } else {
+    res.redirect(`http://${req.get("host")}/?msg=wrong format`);
   }
-  else {
-    res.redirect(`http://${req.get('host')}/?msg=wrong format`);
-  }
-})
+});
 
-app.get('/result/:rid', (req, res) => {
-  client.db("userdatassv").collection("apriori_results").find({ "_id": req.params.rid.toString() }).toArray((error, result) => {
-    if (error) {
-      return res.status(500).send(error);
-    }
-    res.send(result);
-  })
-
-})
+app.get("/result/:rid", (req, res) => {
+  client
+    .db("userdatassv")
+    .collection("apriori_results")
+    .find({ _id: req.params.rid.toString() })
+    .toArray((error, result) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      res.send(result);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Running on Port ${port}`)
+  console.log(`Running on Port ${port}`);
 });
